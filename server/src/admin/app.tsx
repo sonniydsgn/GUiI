dcfb3ee7c1bc1befd6aa0e2d2fb842994febb444
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2025-12-02 01:47:04 +0500

    chore: widget translations

--- server/src/admin/app.tsx
+++ server/src/admin/app.tsx
@@ -16,13 +16,46 @@ export default {
     // https://github.com/strapi/strapi/blob/main/packages/core/upload/admin/src/translations/ru.json
     translations: {
       ru: {
+        // авторизация
         "global.home": "Главная",
+        "HomePage.header.title": "Привет, {name}!",
+        "HomePage.header.subtitle": "Добро пожаловать в панель администратора",
+        // главная
+        "widget.key-statistics.title": "Статистика проекта",
+        "widget.key-statistics.list.admins": "Администраторов",
+        "widget.key-statistics.list.apiTokens": "API токенов",
+        "widget.key-statistics.list.assets": "Файлов",
+        "widget.key-statistics.list.components": "Компонентов",
+        "widget.key-statistics.list.contentTypes": "Типов контента",
+        "widget.key-statistics.list.entries": "Записей",
+        "widget.key-statistics.list.locales": "Языков",
+        "widget.key-statistics.list.webhooks": "Вебхуков",
+        "widget.last-activity.link": "Открыть журнал действий",
+        "widget.last-activity.no-activity": "Действий нет",
+        "widget.last-activity.title": "Последние действия",
+        "widget.profile.title": "Профиль",
+        "content-manager.widget.chart-entries.title": "Запись",
+        "content-manager.widget.chart-entries.count.label": "{count, plural, =0 {Записей} one {Запись} other {Записи}}",
+        "content-manager.widget.chart-entries.tooltip": "{count} {label}",
+        "content-manager.widget.last-edited.title": "Последние измененные записи",
+        "content-manager.widget.last-edited.single-type": "Одиночный тип",
+        "content-manager.widget.last-edited.no-data": "Нет измененных записей",
+        "content-manager.widget.last-published.title": "Последние опубликованные записи",
+        "content-manager.widget.last-published.no-data": "Нет опубликованных записей",
+        "HomePage.addWidget.title": "Добавить виджет",
+        "HomePage.addWidget.noWidgetsAvailable": "Нет доступных виджетов",
+        "HomePage.addWidget.button": "Добавить виджет",
+        "HomePage.widget.delete": "Удалить",
+        "HomePage.widget.drag": "Потяните, чтобы переместить",
+        // авторизация
         "app.components.HomePage.welcome.again": "Привет!",
         "Auth.form.welcome.subtitle": "Войдите в свой аккаунт",
         "Auth.form.welcome.title": "Привет!",
+        // медиа
         "app.components.InputFile.newFile": "Добавить новый файл",
         "app.components.ImgPreview.hint": "Добавьте свои файлы сюда",
         "upload.header.actions.add-assets": "Добавить файлы",
+        // контент-менеджер
         "content-manager.containers.edit.panels.default.title": "Запись",
         "content-manager.actions.clone.error": "Произошла ошибка при дублировании документа",
         "content-manager.actions.clone.label": "Дублировать",
