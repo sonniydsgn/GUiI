dc6125f26dc91a436083fc0e8b289d721e7d89b5
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2025-12-02 00:02:53 +0500

    fix: font, animations, some semantic

--- client/app/components/StatsCard/StatsCard.tsx
+++ client/app/components/StatsCard/StatsCard.tsx
@@ -4,12 +4,12 @@ import type { IStatsCard } from "~/data/StatsCardsData";
 
 const StatsCard = ({ count, characteristic }: IStatsCard) => {
   return (
-    <article className="stats-card">
+    <div className="stats-card">
       <h3 className="stats-card__title">
         <span className="stats-card__count h2">{count}</span>
         <span className="stats-card__characteristic body-l-medium">{characteristic}</span>
       </h3>
-    </article>
+    </div>
   );
 };
 
